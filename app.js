695e5e2c6fdf587dc442dc7175a8ee2b6060faaf
dany ramirez <drz0224@gmail.com>
2023-02-15 22:21:06 -0800

    added post login

--- app.js
+++ app.js
@@ -8,5 +8,25 @@ app.use(express.urlencoded({ extended: true}))
 app.use(cors())
 
 app.get("/", cors(), (req, res) => {
-    
+
+})
+
+app.post("/", async (req , res) => {
+    const {email, password} = req.body
+
+    try{
+        const check = await collectionfindOne({
+            email:email
+        })
+        if(check) {
+            res.json("exist")
+        }
+        else{
+            res.json("notexist")
+        }
+        
+    } 
+    catch(e) {
+            res.json("notexist")
+        }
 })

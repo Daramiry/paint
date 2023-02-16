42039fc42ae0b2c19e93adcba150b22159e8c0b0
dany ramirez <drz0224@gmail.com>
2023-02-15 22:14:32 -0800

    adjusting errors

---
+++ mongo.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+import mongoose from 'mongoose'
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect("mongodb://127.0.0.1:27017/paint")
+.then(() => {
+    console.log("mongo connected")
+})
+.catch(() => {
+    console.log('failed to connect')
+})
+
+const SignupSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+})
+
+const collection = new mongoose.model("signup-info", SignupSchema)
+
+module.exports = collection
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import express from 'express'
 const express = require('express')
 const app = express()
@@ -8,16 +9,18 @@ const react = require("React")
 const users = []
 
 app.set("view engine", 'ejs')
-app.use(express.urlencoded({ extended: false }))
+// app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-    res.render('./index.ejs')
+app.get('/main', (req, res) => {
+    res.render('./main.js')
 })
 
-app.get('/login', (req, res) => {
-    res.render('./login.ejs')
-}) 
+// app.get('/login', (req, res) => {
+//     res.render('./login.ejs')
+// }) 
 
-app.listen(3000, () => {
-    console.log("port connected");
-})
+// app.listen(3000, () => {
+//     console.log("port connected");
+// })
+
+app.listen(3000)
